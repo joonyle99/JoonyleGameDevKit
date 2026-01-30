5187b1edb949d4f6466b564dc320b99d5995e7a6
joonyle99 <gongjunyeol@gmail.com>
2026-01-30 12:22:39 +0900

    fix: remove IState (only remained StateBase)

--- Runtime/State/StateBase.cs
+++ Runtime/State/StateBase.cs
@@ -3,19 +3,9 @@ using UnityEngine;
 namespace JoonyleGameDevKit
 {
     /// <summary>
-    /// 상태 인터페이스
-    /// </summary>
-    public interface IState<T> where T : MonoBehaviour
-    {
-        void Enter(T owner);
-        void Exit(T owner);
-        void Update(T owner);
-    }
-
-    /// <summary>
     /// 상태 기본 클래스
     /// </summary>
-    public abstract class StateBase<T> : IState<T> where T : MonoBehaviour
+    public abstract class StateBase<T> where T : MonoBehaviour
     {
         public abstract void Enter(T owner);
 
