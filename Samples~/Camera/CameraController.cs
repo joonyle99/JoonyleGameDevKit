3128d61e6104754a5577e7159e33ebdeeafe6b9f
joonyle99 <gongjunyeol@gmail.com>
2026-07-30 20:13:58 +0900

    docs(samples): 카메라 컨트롤러 샘플 코드 추가
    CameraControllerBase를 상속한 프로젝트별 CameraController 구현 예제

---
+++ Samples~/Camera/CameraController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace JoonyleGameDevKit
+{
+    public class CameraController : CameraControllerBase
+    {
+        public override void LateTick(float deltaTime)
+        {
+
+        }
+    }
+}
